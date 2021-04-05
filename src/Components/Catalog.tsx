78b4bfef3e0c88bde2decb314f98cdf5c57d27fd
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-04-05 19:45:10 -0300

    add json server and list products

--- src/Components/Catalog.tsx
+++ src/Components/Catalog.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { IProduct } from '../Store/Modules/Cart/Types';
+import api from '../Services/api';
 
 const Components: React.FC = () => {
-    const catalog = useSelector(state => state);
+    const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-    console.log(catalog);
+    useEffect(() => {
+      api.get('products').then(response => {
+        setCatalog(response.data);
+      })
+    })
 
   return (
-    <h1>Catalog</h1>
+    <main>
+      <h1>Catalog</h1>
+      {catalog.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <strong>{product.price}</strong> {" - "}
+
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
   );
 }
 
