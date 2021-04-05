a0172f770894ffd44c385e05af68453efb9c63bf
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-04-05 20:21:01 -0300

    add products to cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './Store/index';
+import Cart from './Components/Cart';
 import Catalog from './Components/Catalog';
 
 const App = () => {
   return(
     <Provider store={store}>
       <Catalog/>
+      <Cart />
     </Provider>
   )
 }
--- src/Store/Modules/Cart/Reducer.ts
+++ src/Store/Modules/Cart/Reducer.ts
@@ -5,8 +5,28 @@ const INITIAL_STATE: ICartState = {
   items: []
 };
 
-const Cart: Reducer<ICartState> = () => {
-  return INITIAL_STATE;
+const Cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
+  switch(action.type){
+    case 'ADD_PRODUCT_TO_CART' : {
+      console.log(action.payload); // Payload = dado enviado pro reducer
+
+      const { product } = action.payload;
+
+      return {
+        ...state,
+        items: [
+          ...state.items,
+          {
+           product,
+           quantity: 1,
+          }
+        ]
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 }
 
 export default Cart;
