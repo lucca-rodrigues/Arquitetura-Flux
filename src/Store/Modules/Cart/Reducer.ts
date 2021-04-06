6bfed0d56a6d7139a3dd8a78db32b094e64b3b7c
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-04-05 21:20:27 -0300

    add immer

--- src/Store/Modules/Cart/Reducer.ts
+++ src/Store/Modules/Cart/Reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from "redux";
+import produce from 'immer';
 import { ICartState } from "./Types";
 
 const INITIAL_STATE: ICartState = {
@@ -6,27 +7,24 @@ const INITIAL_STATE: ICartState = {
 };
 
 const Cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch(action.type){
-    case 'ADD_PRODUCT_TO_CART' : {
-      console.log(action.payload);
+  return produce(state, draft => {
+    switch(action.type){
+      case 'ADD_PRODUCT_TO_CART' : {
+        console.log(action.payload);
+        const { product } = action.payload;
 
-      const { product } = action.payload;
+          draft.items.push({
+            product,
+            quantity: 1,
+          });
 
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
-           product,
-           quantity: 1,
-          }
-        ]
-      };
+          break;
+      }
+      default: {
+        return state;
+      }
     }
-    default: {
-      return state;
-    }
-  }
+  });
 }
 
 export default Cart;
