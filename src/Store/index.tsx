9bfb20158a5ee8e06d84b74cb40e13b61d35abb3
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-04-06 13:44:15 -0300

    add devtools debugger

--- src/Store/index.tsx
+++ src/Store/index.tsx
@@ -1,10 +1,14 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { ICartState } from './Modules/Cart/Types';
 import RootReducer from './Modules/RootReducer';
 
 export interface IState {
   cart: ICartState;
 }
-const store = createStore(RootReducer);
+const store = createStore(
+  RootReducer,
+  composeWithDevTools(),
+);
 
 export default store;
